fef7fbdaa8e3d698b3e6ade959198dfeca1c5b61
Bert0ns <121551954+Bert0ns@users.noreply.github.com>
2025-03-26 13:23:10 +0100

    Update wheel_of_fortune.ino

--- wheel_of_fortune/wheel_of_fortune.ino
+++ wheel_of_fortune/wheel_of_fortune.ino
@@ -1,15 +1,16 @@
 #include <FastLED.h>
 
-#define NUM_LEDS (144 * 2) // 2 metri * 144 LED/metro
+#define NUM_LEDS (144 * 2)  // 2 metri * 144 LED/metro
+#define MAX_LED_NUMBER_SELECTED (NUM_LEDS * 5)
 #define DATA_PIN (3)
 #define BUTTON_PIN (4)
-#define LEDS_PER_STEP (10) // Lunghezza della porzione illuminata
+#define LEDS_PER_STEP (10)  // Lunghezza della porzione illuminata
 
 CRGB leds[NUM_LEDS];
 
 enum State {
-  STATE_IDLE,
-  STATE_ROTATING
+    STATE_IDLE,
+    STATE_ROTATING
 };
 
 State currentState = STATE_IDLE;
@@ -17,81 +18,74 @@ int buttonState = 0;
 int lastButtonState = 0;
 int rotationPosition = 0;
 char ledBrightness = 100;
+int randomLedSelected = 1;
+
+const CRGB colorViolaFesta = CRGB(255, 0, 157);
+const CRGB colorBluFesta = CRGB(255, 0, 157);
 
 void setup() {
-  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS); //WS2815 è compatibile con WS2812B -> collegare i due data pin insieme
-  pinMode(BUTTON_PIN, INPUT);
+    FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);  // WS2815 è compatibile con WS2812B -> collegare i due data pin insieme
+    pinMode(BUTTON_PIN, INPUT);
 
-  FastLED.setBrightness(ledBrightness);
-  FastLED.clear();
-  FastLED.show();
-}
-
-void loop() {
-  readAllInputs();
-
-  switch (currentState) {
-    case STATE_IDLE:
-      handleIdleState();
-      handleIdleStateTransitions();
-      break;
-    case STATE_ROTATING:
-      handleRotatingState();
-      handleRotatingStateTransitions();
-      break;
-  }
+    FastLED.setBrightness(ledBrightness);
+    FastLED.clear();
+    FastLED.show();
 }
 
 void readAllInputs() {
-  buttonState = digitalRead(BUTTON_PIN);
+    buttonState = digitalRead(BUTTON_PIN);
 }
 
-void handleIdleState() {
-  FastLED.clear();
-  //fill_noise16(leds, NUM_LEDS, 8, 8, 8, 8, 8, 8, 8);
-  //fill_rainbow(leds, NUM_LEDS, 0);
-  FastLED.show();
+void transitionToStateIdle() {
+    currentState = STATE_IDLE;
+
+    // set output for idle state
+    FastLED.clear();
+
+    fill_gradient_RGB(leds, NUM_LEDS, colorViolaFesta, colorBluFesta, colorViolaFesta, colorBluFesta);
+    // fill_noise16(leds, NUM_LEDS, 8, 8, 8, 8, 8, 8, 8);
+    // fill_rainbow(leds, NUM_LEDS, 0);
+    FastLED.show();
 }
 
 void handleIdleStateTransitions() {
-  if (lastButtonState == LOW && buttonState == HIGH) { // Pulsante premuto, fronte salita
-      currentState = STATE_ROTATING;
+    if (lastButtonState == LOW && buttonState == HIGH) {  // Pulsante premuto, fronte salita
+        currentState = STATE_ROTATING;
+        randomLedSelected = random(NUM_LEDS, MAX_LED_NUMBER_SELECTED);
     }
-  lastButtonState = buttonState;
+    lastButtonState = buttonState;
 }
 
 void handleRotatingState() {
-  /*
-  for (int i = 0; i < NUM_LEDS; i++) {
-    updateRotation();
-    FastLED.show();
-    delay(10); // Velocità di rotazione
-  }
-  */
-  uint8_t hue = 0;
-	for(int i = 0; i < NUM_LEDS; i++) {
-		// Set the i'th led to red 
-		leds[i] = CHSV(hue++, 255, 255);
-		// Show the leds
-		FastLED.show(); 
-		// now that we've shown the leds, reset the i'th led to black
-		//leds[i] = CRGB::Black;
-		fadeall();
-	}
+    uint8_t hue = 0;
+    for (int i = 0; i < randomLedSelected; i++) {
+        leds[i % NUM_LEDS] = CHSV(hue, 255, 255);
+
+        FastLED.show();
+
+        // leds[i] = CRGB::Black;
+        fadeall();
+
+        hue = (hue + 1) % 255;
+    }
 }
 
-void handleRotatingStateTransitions(){
-  currentState = STATE_IDLE; // Torna allo stato IDLE dopo la rotazione
+void fadeall() {
+    for (int i = 0; i < NUM_LEDS; i++) {
+        leds[i].nscale8(250);
+    }
 }
 
-void updateRotation() {
-  fill_solid(leds, NUM_LEDS, CRGB::Black); // Spegni tutti i LED
+void loop() {
+    readAllInputs();
 
-  for (int i = 0; i < LEDS_PER_STEP; i++) {
-    leds[(rotationPosition + i) % NUM_LEDS] = CRGB::BlueViolet; // Accendi la porzione
-  }
-
-  rotationPosition = (rotationPosition + 1) % NUM_LEDS; // Aggiorna la posizione
+    switch (currentState) {
+        case STATE_IDLE:
+            handleIdleStateTransitions();
+            break;
+        case STATE_ROTATING:
+            handleRotatingState();
+            transitionToStateIdle();
+            break;
+    }
 }
-
-void fadeall() { for(int i = 0; i < NUM_LEDS; i++) { leds[i].nscale8(250); } }
